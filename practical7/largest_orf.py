d774c6d0cff3a71c461c7bb110e115d2c8fb0872
Shuwen25 <shuwen.25@intl.zju.edu.cn>
2026-04-01 14:21:04 +0800

    Update largest_orf.py

--- practical7/largest_orf.py
+++ practical7/largest_orf.py
@@ -6,17 +6,20 @@ stop_codons=['UAA','UAG','UGA']
 orf_dict={}
 #traverse the sequence to find all the start codon 'AUG' 
 seq_length=len(seq)
-for start_idx in range(seq_length-2):
-    #check if the triplet start from the current index is the start codon
-    for current_idx in range(start_idx,seq_length-2,3):
-        current_triplet=seq[current_idx:current_idx+3]
-        #when the stop codon is found, cut the ORF sequence
-        if current_triplet in stop_codons:
-            orf_seq=seq[start_idx:current_idx+3]
-            orf_length=len(orf_seq)
-            orf_dict[orf_seq]=orf_length
-            #break when the first stop codon is found
-            break
+for start_point in range(seq_length-2):
+    for current_point in range(start_point,seq_length-2,3):
+        triplet=seq[current_point:current_point+3]
+        if triplet==start_codon:
+            #check if the triplet start from the current index is the start codon
+            for current_idx in range(current_point,seq_length-2,3):
+                current_triplet=seq[current_idx:current_idx+3]
+                #when the stop codon is found, cut the ORF sequence
+                if current_triplet in stop_codons:
+                    orf_seq=seq[current_point:current_idx+3]
+                    orf_length=len(orf_seq)
+                    orf_dict[orf_seq]=orf_length
+                    #break when the first stop codon is found
+                    break
 #select the largest ORF
 if orf_dict:
     largest_orf=max(orf_dict,key=orf_dict.get)
